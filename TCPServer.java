9e3684cf252014ff6475027c8fc713f82982aaac
DanielRivera0804 <daniel.riverat@alumnos.usm.cl>
2014-06-05 22:16:33 -0400

    Update TCPServer.java

--- TCPServer.java
+++ TCPServer.java
@@ -4,16 +4,39 @@ import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-public class TCPServer {
+public class TCPServer implements Runnable {
+	
+	static String flag;
 	
 	public static void main(String argv[]) throws Exception
-	{
-		String clientSentence;
-		String capitalizedSentence;
+	{		
+		flag = "1";
+		(new Thread(new TCPServer())).start();
+		Thread.sleep(2000);
+		flag = "2";
+		(new Thread(new TCPServer())).start();
+				
+	}
+	
+	public void run(){
 		
+		if(flag.equals("1")){
+			try {
+				TCPServer1();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();			}		
+		}	
+		if(flag.equals("2")){
+			TCPServer2();
+		}
+	}
+
+	private static void TCPServer1() throws IOException {
+		String clientSentence;
+		String capitalizedSentence;		
 		// Establece el servidor en el socket 6789
 		ServerSocket welcomeSocket = new ServerSocket(6789);
-		
 		//Se ejecuta un bucle de escucha/acepta
 		while(true)
 		{
@@ -25,23 +48,64 @@ public class TCPServer {
 			String clientSentences[] = clientSentence.split(":");
 			String comando = clientSentences[1];
 			
+			String[] comandos = clientSentences[0].split("=");
+			String thecomando = comandos[0];
 			String cadenaHistorial = SolicitudHistorial();
-			System.out.println("CADENAHISTORIAL: "+ cadenaHistorial);
-			if(comando.equals("actualizar"))
-				outToClient.writeBytes(clientSentences[0]+ "#" + cadenaHistorial);
-			else if(comando.equals("enviar"))
-				actualizarHistorial(clientSentences[0]);
-			
+				if(comando.equals("actualizar"))
+					outToClient.writeBytes(clientSentences[0]+ "#" + cadenaHistorial);
+				else if(comando.equals("enviar") && (thecomando.equals("message")))
+					actualizarHistorial(clientSentences[0]);
+	
 			System.out.println("Received: " + clientSentences[1]);	
 			capitalizedSentence = clientSentence.toUpperCase() + '\n';    
 			outToClient.writeBytes(capitalizedSentence);
 		}
+		
 	}
+	
+	private static void TCPServer2() {
+		ServerSocket server;
+		 Socket connection;
+		 
+		DataOutputStream output;
+		 BufferedInputStream bis;
+		 BufferedOutputStream bos;
+		 
+		byte[] receivedData;
+		 int in;
+		 String file;
+		 
+		try{
+		 //Servidor Socket en el puerto 5000
+		 server = new ServerSocket( 6788 );
+		 while ( true ) {
+		 //Aceptar conexiones
+		 connection = server.accept();
+		 //Buffer de 1024 bytes
+		 receivedData = new byte[1024];
+		 bis = new BufferedInputStream(connection.getInputStream());
+		 DataInputStream dis=new DataInputStream(connection.getInputStream());
+		 //Recibimos el nombre del fichero
+		 file = dis.readUTF();
+		 file = file.substring(file.indexOf('\\')+1,file.length());
+		 //Para guardar fichero recibido
+		 bos = new BufferedOutputStream(new FileOutputStream(file));
+		 while ((in = bis.read(receivedData)) != -1){
+		 bos.write(receivedData,0,in);
+		 }
+		 bos.close();
+		 dis.close();
+		 }
+		 }catch (Exception e ) {
+		 System.err.println(e);
+		 }
+		
+	}
+
 
 	private static void actualizarHistorial(String query) throws IOException {
 		
 		System.out.println("ACTUALIZAR HISTORIAL");
-		
 		String[] s = query.split("=");
 		String[] datos = s[1].split("\\+");
 		String comando = datos[0];
@@ -74,7 +138,6 @@ private static String SolicitudHistorial() throws IOException {
 		while( cadena != null)
 		{
 			cadena = br.readLine();
-			System.out.println(cadena);
 			if( cadena != null){	
 				cadenaHistorial = cadenaHistorial.concat(cadena + "$");
 			}
